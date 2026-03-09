8df27c70ac08cbfcc1828980a42f3dd4fef22d3f
MentormeShe <shivani@mentormecollective.org>
2026-03-08 21:10:44 -0400

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// Change navbar background on scroll
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".navbar");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
+});
